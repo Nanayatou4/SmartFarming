498b4094893e893e3fcab90f36e446b7cffb0f0c
Soumayatou <dienaannasoumayatou@gmail.com>
2025-02-27 02:23:45 +0000

    Update

--- src/main/java/com/project/elevage/intelligent/Smart_Farming/repository/EleveurRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.elevage.intelligent.Smart_Farming.repository;
-
-import com.project.elevage.intelligent.Smart_Farming.model.Eleveur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EleveurRepository extends JpaRepository<Eleveur, Long> {
-}
